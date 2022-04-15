30d467a308d79fb1b782d1760415cad16813ec94
Oskar <oskar.henriksson@chas.se>
2022-04-15 16:14:20 +0200

    Init commit

---
+++ src/main/java/com/example/JenkinsExperiment/controllers/controller.java
@@ -0,0 +1,33 @@
+package com.example.JenkinsExperiment.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/tea")
+public class controller {
+
+
+    @GetMapping(path="/get")
+    public ResponseEntity<String> getStudent() {
+        return new ResponseEntity<>("Basic get response", HttpStatus.OK);
+    }
+
+    @GetMapping(path="/tea")
+    public ResponseEntity<String> tea(){
+        return new ResponseEntity<>("Soooo.... is this gonna work?... if yes, react-pipe, then break", HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    @GetMapping(path="/environment")
+    public ResponseEntity<String> environment(){
+        String environment = System.getProperty("JAVA_HOME");
+        System.out.println(environment);
+
+
+        return new ResponseEntity<>("Soooo.... is this gonna work?... if yes, react-pipe, then break", HttpStatus.OK);
+    }
+
+}
