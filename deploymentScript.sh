3d303161cf9484c23bcb6aaaba18edf4ac36f08e
Oskar <oskar.henriksson@chas.se>
2022-04-18 12:14:21 +0200

    message

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -4,39 +4,8 @@ pid=$(lsof -t -i :4242 -s TCP:LISTEN)
 echo "pid: $pid"
 
 if [ -n "$pid" ]; then
-  echo "Shutting $pid down"
+  echo "Found listener at port 4242. Shutting $pid down"
   kill -15 $pid
 else
-  echo "doing nothing"
+  echo "No listener found at port 4242"
 fi
-
-
-
-
-
-
-
-
-#mvn clean compile exec:java &
-
-#export JENKINS_NODE_COOKIE=dontKillMe
-#export BUILD_ID=dontKill
-
-#echo "jen node: $JENKINS_NODE_COOKIE"
-#echo "buildid: $BUILD_ID"
-
-#echo "running deploymentScript.sh"
-
-#echo "lsof: $(lsof -t -i :4242 -s TCP:LISTEN)"
-
-#export JENKINS_NODE_COOKIE=dontKillMe && java -jar ./target/JenkinsExperiment-spring-boot.jar >> ./output.logs &
-
-
-#pid=$(lsof -t -i :4242 -s TCP:LISTEN)
-#echo "pid: $pid"
-
-#echo "ls"
-#ls
-#
-#echo "pwd"
-#pwd
